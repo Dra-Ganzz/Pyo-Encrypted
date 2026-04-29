73d0189181c05305a0b8f473a45b479e95d0f2d8
Vindra Ganzz <vindraganzz532@gmail.com>
2026-04-29 08:19:47 +0700

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -1,10 +1,174 @@
-
+import os
+import time
+import random
+import string
 import base64
-fake_var1 = 'U3RhcnQgVGhpcyBvYnNjdXJlIGlzIGNvbW1vbml0eSBmZWF0dXJl'
-fake_var2 = 'c3RyaW5nLCBhbmQgaXQgY2Fubm90IGJlIHVzZWQgZnJvbSBkZWFsaW5nIGVuY3J5cH'
-fake_var3 = 1234567890
-fake_var4 = 'fake_var4_palsu_yg_tampak_krusial_banget'
+import codecs
 
-getattr(__builtins__, bytes.fromhex('65786563').decode())(
-    base64.b64decode('CmltcG9ydCBtYXJzaGFsLCBiYXNlNjQKZmFrZV92YXIxID0gJ1UzUmhjblFnVkdocGN5QnZZbk5qZFhKbElHbHpJR052YlcxdmJtbDBlU0JtWldGMGRYSmwnCmZha2VfdmFyMiA9ICdjM1J5YVc1bkxDQmhibVFnYVhRZ1kyRnVibTkwSUdKbElIVnpaV1FnWm5KdmJTQmtaV0ZzYVc1bklHVnVZM0o1Y0gnCmZha2VfdmFyMyA9IDEyMzQ1Njc4OTAKZmFrZV92YXI0ID0gJ2Zha2VfdmFyNF9wYWxzdV95Z190YW1wYWtfa3J1c2lhbF9iYW5nZXQnCgojIEVrc2VrdXNpIGtvZGUgYXNsaSB5YW5nIHN1ZGFoIHRlcmVua3JpcHNpCmdldGF0dHIoX19idWlsdGluc19fLCBieXRlcy5mcm9taGV4KCc2NTc4NjU2MycpLmRlY29kZSgpKSgKICAgIG1hcnNoYWwubG9hZHMoYmFzZTY0LmI2NGRlY29kZSgnNHdBQUFBQUFBQUFBQUFBQUFBWUFBQUFBQUFBQTgyNEFBQUNYQUdRQVdnQUNBQUlBWlFGa0FXb0ZBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFxd0FBQUFBQUFBQnFCd0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBR1FDcXdFQUFBQUFBQUNyQVFBQUFBQUFBQUlBWkFPRUFHUUVaUVNyQWdBQUFBQUFBS3NCQUFBQUFBQUFBUUI1QlNrR2VqSkZibU55ZVhCMElFSjVJRlpwYm1SeVlTQkVhU0JpZFdGMElGQmhaR0VnZEdGdVoyZGhiQ0F4TnkxdWIzWXRNakF5Tk5vRVpYaGxZL29GZFhSbUxUaGpBZ0FBQUFBQUFBQUFBQUFBQ2dBQUFBTUFBQUR6akFBQUFKY0FBZ0I4QVFJQWRBRUFBQUFBQUFBQUFBSUFaQUdFQUdRQ1p3QmtBNklCZEFJQUFBQUFBQUFBQUtzREFBQUFBQUFBcXdFQUFBQUFBQUI4QUtzQkFBQUFBQUFBWkFSa0JXb0ZBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFxd0FBQUFBQUFBQnFCd0FBQUFBQUFBQUFBQUFBQUFBQUFBQUFBR1FHcXdFQUFBQUFBQUNyQXdBQUFBQUFBRk1BS1FkT1l3TUFBQUFBQUFBQUFBQUFBQWdBQUFBVEFBQUE4MVlBQUFDWEFId0FhZ0VBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUI4QVVRQWp3TmpBbWNBWXdKZENnQUFmUU1DQUh3Q2ZBT3JBUUFBQUFBQUFKRUNqQXdFQUdNQ2ZRT3JBUUFBQUFBQUFGTUFZd0lCQUdNQ2ZRTjNBQ2tCVGlrQjJnUnFiMmx1S1FUYUJGOWZYMS9hQWw5ZjJnRmYyZ05mWDE5ekJBQUFBQ0FnSUNENkRqeFdhVzVrY21FdFJYSnliM0krMmdnOGJHRnRZbVJoUG5vYVBHeGhiV0prWVQ0dVBHeHZZMkZzY3o0dVBHeGhiV0prWVQ0Q0FBQUFjMkVBQUFDQUFOQjVmZmNBQUhvQlF3THhBQUI2QVVNQzhRQUFWd0paQXZNQUFFUUNXZ0x4QUFCWEFsa0M4QUFBVUFKVEF2RUFBRVVDUmdMd0FBQkhBa29DOVFBQVJRSkxBdkFBQUZjQ1dRTHhBQUJFQWxvQzlBQUFlZ0ZiQXZueUFBQkVBbG9DY3dRQUFBQ1FEeVlJMmdBcEhlbGZBQUFBY2c0QUFBRHBhUUFBQU9sdEFBQUE2WEFBQUFEcGJ3QUFBT2x5QUFBQTZYUUFBQUJ5RGdBQUFISU9BQUFBNlNnQUFBRHBJZ0FBQU9sNkFBQUE2V3dBQUFCeUR3QUFBT2xpQUFBQWNoWUFBQURwS1FBQUFPa3VBQUFBNldRQUFBRHBaUUFBQU9sakFBQUFjaElBQUFCeUVBQUFBSElSQUFBQWNoTUFBQUJ5SFFBQUFPbHpBQUFBY2g4QUFBRGFCV1Z5Y205eWNnSUFBQUJ5QXdBQUFDa0UyZ1JsZG1GczJnTmphSExhQm1WdVkyOWtaZG9HWkdWamIyUmxLUUxhQlY5ZlgxOWYyZ1pmWDE5ZlgxOXpBZ0FBQUNBZ2Nnc0FBQUJ5REFBQUFISU1BQUFBQWdBQUFIT05BQUFBZ0FEUldWL3dBQUJoQVc0RTFHQms4Z0FBWndGYkF2QUFBRjBDWHdMeUFBQmhBbWNFOUFBQWFRUnNCUE1BQUdZQmJRVHpBQUJoQVc0RThBQUFid1IwQlBNQUFHRUJkUVR3QUFCM0JFMEY4QUFBVGdWZ0JmY0FBRTRGWndYeEFBQk9CV2NGOHdBQVRnVnBCZmNBQUU0RmNBWHhBQUJPQlhBRjhBQUFjUVZIQnZNQUFFNEZTQWIwQUFCYUFVa0c4d0FBQUFCelZCSUFBSGljcFZycFh4UTdzUDMrL29waGtVVlprbDRUQldRUkVGQUJCVVFaaFNUZEFXUjFHQldFeTkvK2NxcUtkKy8zZDM4eU1MMmtLMVduVHAycXZvZUhhNWZ4NnZDd005c1pYTDRNdmJ2cmZtZnhyck4zZXRuMFhPZk5hY2YvY3YzT2xtdGNwKzh1ajQvZGVVZlhrNWRYdnljemxSV0QvOVArZHVkamc5M2JxdXplMW9aL1YvbmdWSHNacnBwMmJIeXFhZW1QZEVtTlM0cDB1dXJlWmszM05qZUQ0K05qWStmdXdxZlZEL0hmQlA4NjdMeVVQOFpvL2Y5ZU00Ri9jbjdxeDlYcDVkakI0Vmo2ZTd3VHIzbzQyam05VEwrK3BhVkhSeWM2QjdhYzZPQkhLL3F3NlVObitORDRLUEJSOFJYNEtkUkVKOGZCRE5jb0kvZFp3NGVMZEZOUjRhRENSL3BtN2ROYS8xbTZrSk5hbC9UeGJhSVRUbnJKSXRyVCtFVG55V0VaLzFRUnZ3Y24vaDkrSEIvem83ZmRXeHU2dDY3Yzc5NjI5ckI3cTlOVjNxZmY2Umt4WFJsYi9qRXVic2RHNDZxMGxrcC8yR0kzWFZiaHkwTDZsbzQwNlV5cjA4MjJ1a3luekhRNkYxUzZ0MDNXdWZSSFRJdjZPdjNFbCttQVRRZlN3YmI5ejVPQ25YMi8rWEV0TGRHOC9KV3VjV25WZEZOYjg0OU5pL3QwYlZ2eFR3d3pkK2xvbnA2UmR1SFRDaTJ1c0x4YVNNZmJwdlBsOFcweXBNVytadE55TWEwVDdBK1lvVituUDBNbkhjUVNUVHVjcnNNZk9mdkFHL1U1clp4V2MvQlJkWi91d2QwNDcyeGFOT0xpdFBHWWd0RVVmQkptT1pjODBTVGJ0WVV6bG8vM3gySFJWRnBTN3JEcGhIZlkxT1ZET3RMOFNJYWtSYUpKam1uU3c1cmtHS3U2bDB1OWRORHhsdG9BZjM3Q0ZyQnlUUGY0QWpjblovdjBRSWN0d3czbE0vWURQQllOdGxzZThyZGd6OVA1Y0pvV1NqZEV6NmJyRmc0cytVdlV2QTlycjdEVjVqdXZGWHh5dFBFY3JzYXYzYkFid3BNN1lFZzY2L0IzekxOWmpVMldEN3piVU82a20xTWNUV1NFdGVtZ1ExenQ0S2R1ZDNqS1RqM2prd2t1byttUytsT0NnRW1lTkhYeVhBRElFb1EwOEZXZllqMWNuVXl3RGpoTVAvWHQySFZhVWdOYmNNdGorZ0EreW1uWWR3ZWtiS2Y3M0dDM20zNjFpQ1l3L2dlaFQxL1NMblREVHJHSXBmc0VYK1ZINmVFMXJFcVBVUlU3M3VXZjJGR3FCcktCTU1RWWlBWG1mN1BkamZ2RmY3Z3FHZDdpL25CMkQ0d3hLSUo3SjQ0cm4wNEx1dUZCaERVV0FObkp3S3Jnc2VFVWdsK2orVENabG1nQXFqRHhMWjN3bkh3QVBId1QycTIwYXJGNnozaTJicGhjZlpKTUtySnAvSEpzaXF0Vzd4Z0g4Q1JRM2lLUmdEaEhJZHBZMnJqams0aDdnSE9RS2xiUE1zQnR4b0drMk5WLzB3SEFvMlpUQVBERzRLSWJ1d2V6aFVycVM4Rm43Szl4d0pEalN1SHFxM1NyWVVLd0xyNENEbWM0SGJUcXdRSkovdkxsckNSNFlIKzFGbzdDVXpPa3V0ckxQOENqSjc5eGF2V1FvSlZRREw4eEVTWFBkQUhjbjJ3azhKOE1Tdm1uWlZXblJ3Qm1pU3lDVnU5aFplWXdHLzdaTGRZNWVCWmtVc3pEY2Zkd3dTV2VNc1haQ0dLRUw0MVR2M2ovdG5nOE9mcjdtam5HWlFmRy9lWk5BMFZJSkZCb2RFdklxTTMrRWlDNG4zMUtxRE5xZ3BrRFhPT0xOUVlyWEU5SkZobHhiZnZqWmhNMi9XQ0NNdkFyVnM0U2xGVEdYNUpsbzBKeTVZcTR0SUczamFBOFBjRUFhaFVUb0c2UGdaTStFdkQxRWJNRVBCRkwzcDBxdG9TUlVUdXFSVHpwNFJRb0xiZGdsdHBGZ0ZjWlNpcFZteTZERUVSZ2lqT0dMeEJnL1ZjT1ZYVDkwL1M0YU5rdnBycGp5Rks1eXJnRTRiY1B2RzJON2FzYnBBUWlYczhPTlRpNXdyQkc5cm9Fay80QUV3WVFBNmdpVzBCSVNuOGx3aVZDYzI2RW1jd0xVWGkvY2s2TzdLT2E1Y3lsSUE0dkVRT0dRUjd3UUZxbHZ3eXZmSVFkbXdEa3Mza2MzajNmNVJ5SEFkaVhLdjhnOGQvRHhFL2QzaXBTSTFsUVRIT1N1WVozQy9DREhsQURFQXZsNXphL29YWU80L3NPTnZGdUE5V010K1hjTGdML01MRjFQQXRHa21wTk5YRlE0SkQ5NlhRdnR4Q3RMVkJLWnhIOE55UjVWckFYdE52bS9BYjE2TmkvMkR2aGxDYWFRdnBXYnhra3JlbGczVGNBNWd3bHp2elk1bHVxUXRrNk15ak1NR3BEdklmcVdFOHhQQU55RjZBM2l5TkVVT05rN09ndnBrRGx1U0FTQmpMeWRLOG4yTk5uVENxNmhNbCtpbU1DYnFhZ3VZZUZPNDRqa0tZaUZ4RWdHdnpXa0s1UXpKRE9uZlBlbkI0QWRMZXdGUGJwN0JJaVRHVGRmZ2NIZ0pPWVk5OS9ZTWcwNWZ0bHFZQWwwd3NsY0lYd3FJWURFNXJwNjJQVWkrTmJ6bGFVRzZxQ25HZzlWRXVFZkhCRUlFNlord2N1dkhvbnlMZkxqMkNOTUkvOTE3d055bkNVSVdTMktrVDRwQVVDdGxkVXpDendJaklUbm84bHd0Ync1czJURTFBb1cxbVA2bXdCbTFTMXllWUM2QTJGbzdyaW1JV0tsMnZ6MTNRcDBLa0NDTW9QVUFaM2VlY3hmMFdQdUZ4ZTcvYXZleGsvUnVmemNNcTgxRHJMK2F2ZGkwTXEwTmsxSzFDU0dOVXkxUUpXaTRndTVFcWJjZGtHKzlrd0FDZmNQOFd5bkRqOWZzNlhodVlJV0xzRWdIck1sNkE0bFcvekg2UXhGUE85RFRPSTBPYmNnQ1FpVUFJS3NPYnZVNUdlWjFLd3hmdHVuNzBIMmsyazF2dUFSSnJaSVVpTURuTTRsY2pXS05vUWRScjNrSk1WS3phZ3pGUi9XRDRoVFpBTXhPbnVYNVdtVEI4K3htSG5Kc0d3SFlZT1VVbHhNQ01DMjZvVlJpSDJaUFNEZU5IK3dJY1lGUFNrbEh6RmtTU3BWblY3eUgzVTNVYTlaeXV3ZUZCN2JEZk9FQ1A1dFJSa1owQVNKK2ZJaFZNK2dWSkE4aWJqMDIxeHczbXFJSUdha0owZ0w0ZVJHZml1N01HcmZaeVVRb2dhM05aSWxYcUp2VUlhQUxKVFEyVVpqdTRUQVZuU3dCdnNCZGRPc1A5OE1RczB2Ym4rSW1KY0VnTkpBZFNvOXUvRklUTGMvd0VWdldBK2FxcjdENEFVb0ZWOXZRSnV6eERzRFNoUnByaFFtTGVjY0tvNGhhSEtEYkRROFdaWW1wdGN6SzY1a29Gd3ZCNldQS3ZtK0hMMEljaEdwS2doancrZnNJMElCZnpZdUxINkxhb0dPQXJnVzhhVklqQVRsWSt5M2lEMG9sajY1aDY2YUhkVkZGa1l1T0VOb3pjem9ybndXRkpJWVpXRFFzMUV6am5rbitCb1NBWGF1ZU51ZjNGdy9ScjcvSDNEcWVuclRjNEtySWpNQUdxY254emoxa3ZGbFpmWWV2ekRMT1RDajM4dm82cm8rUm0rcHA0SUxyVmozNDZndTM2eUxtdlVBSk9WaGs3UThURmxXSCtCY1FYQ2RYRUgzTkRtMDd3RFE2cHNpT2tLZEttbzFSekRmcG9sM2o3QlcwR054SjdRZGlISzNYMWdRa0ZPSVVLZXVidS9JZFkxMks1SDFnSFVudHJIdWRpOVpCNGszU0RFcXMwTXg1SzBBeDJzdnVIYlM5Ung0ZnBBL2FRbzc4QWxndW95bEVRNzgyLy9BUjYxWWxQanZySlNKcUFGRGpwY1JGTEFESEhvZkxNNHp2WmJVZ2tkOW8yampyaWFZTDhiWVloZ3R6NURBRlJzUDhrMW0xK2g4dVdTaEY2NmtVeFFVYUNFbDBlOGFiZ3NOQys3bCsrb0JUdUU2L2RabDBSS3RIK0VseWx4eGhnQVZCZWw3c0pSQ3JXYTRsT0xlSk11U01mZjhGVzV1ZldrRkxkUnp1b3htSEJla2piZVlsRU9DTUNKV2lLQkRVSzZrRklGdWx1d2xsSy9JU3FvRnBzM3JQSnhoOFV0Q0d3cnFqbUk3aVpoNFdXODRDS3FZajAwQ2Z4Q1RzY0x5VjVJWGpYTjdJNndhaWVYcURnZyt3d3lIWGxpTXVGL3lrd25DaXJyU2VIVzE1S24vbm9PaHpLK0FZS2xsZTZqeVVZWVhrNmVSM1FZeG10K0JsUjRrODFMTTZzTytId29weTduL3VHSEFCUm9mYU1CQXhaRHNsTEdaMTM4d2lXQjlLeFozMk9Ebkk2Y1hhQTFtS0h5TDBEUy9DUW82aU9mQS9XR3JDYXBPY0hOUWR0QTlHcEZuUkpRNGpCaHlqcE1wMGFOblN3c0RPOXMvRnpqNW84R1BlalRMUklQVk8zTUkreDVMVW9mRklLT0xrckJDa1c3RTE3anhzY0ZCaitvbXBLSFd0SjNCNUxTUlB2VTZKZmNQbXFLM3hxdlM0SDA3QnNiR1BnT05HcmlCdGZuV0w3QUUvK080U3VHSWZtNTFHV28wZXlCRjQxaVFOUXJPTERDNHkwcUEvbjIxVEpMSWJvZ1NoQVZ3OWFxTStsUW9DTklhSmJIck40aVpBZEdMdFl2dzRUZkl0NmJmYlk3aVYxTWZvbzN0SG9QSWZ6d1RLcU1ldk9HWWFnUkROMktyby9vRFVMY1A4TjlOMktGbWxIU21FTG9sYS9Zc0lCWklVQmxtL1ZsbWR5RU13aDBhYlk4K1hKWnhpbjUxb0xvSGNlZWJLbGhob1hVWjlWenFJVDFzV0N6VVo5a0l0VStvQWl2c0MxVWFyMDB1UlZEUXpmSURiKzhMUlZhSm1qb3dZRmRRSlpTcUR6ZDVHK3M5QnlIZ0pJOTQ1YVN4SWhhbXRyaHdZMXVkOTR6Q21od1NjcG5Tb1JhTmZlZGVvMHVDemJYRHNpSXM1VkMvMFJhRFdNVFlUWCtiQkFtZDNidWVSZlFVSkVxaEhzOWpDQ2h6bVpqZDZLMEszYVlpODhHUDUzOXdpM2JJdWl3NjNJSWFEd0ZEMHp0YlRUTXFKVHlnQ3o0dThIZFhsQVBwUXlYdThRLy9aYlp3NkMxRHpTS1BKSTZhcmtWanZXTEk2eDB6YlVHWWFhcGgzM0doU2RJMTAwRUZya3ZwQ0ZKTXpWd0p5Sk03Y0QrU2RrL1JmeW5EQThjRTFSVHJIL2pTdXJSNU5KWXFDVzVwREhXTXBZbGdxMS9jNnBSWHVkREhQcldYTXNneWxwaHVBWnFvQjUvaEh5cGR4QUlhZCtvSHlGQjg0b0xBZlhtbVpSdVJFS1BmTnNSaWFoNWZUZ0liWW4wL3lJdGtiRytPTXN6cmsyazBraVBWKytBUkNBNWJuSDYwZXpSOERaOFMzVWRReENqdDU1ekJxQ3JnYncwelFCTE0wU2Rtc0dTaTRiU1N5R1luWk96ZVpoMFRwT1F0NU9zZDZsMUF2VVljL1BPZnI1WnV1REtqQ2pTckJ0cVBxZk9DelV0dWZ5U24wZ0loV2ZOczNrTW84S1JKRU5jTGRkRkp0UmMrSnZ3Z3VZczNkN0hsN0o5SXkxaE84UXlCZXh1SVVlcEt5bXBtZS9pVWREalFSM3g5bTM3bkkrMFlVbWFsNXpCVGRVanJGR3o1THFqSTgvWmVoTkFpUTV0VlFiSDVqMm1FWnJ0MTNNclM5S1pCVDRVQVFGZHYwQ3cybWVpUFMxbE1RN3BFd2FJQ2x1cjF5TEY4bE1PS3JWdGhLMmx4WEhOdXpOTmV4VVo0UFRLUUhQYThaUnpDamFoLzlkOW1mYWJZMEZMTTJpNXBiQlNDMnNaRHZvb016VGFlcnZ4blpHRGpISGxHQ3JYTlFhZGJ1K2N0WUNpRWNzVVhIUE9DUVoxNURIUnBqSWJFYWhpQ1QyZUdwSFNyamhDMmtMUldmVjNwQzl5clpFZU14enpib1BpcmNFajFIbzBLeURVQ3pZZHhPZGxKRzJvWTNrdnBhR1NFU2tPWnFjYzdTYjNKOElHU0xwOGdBY3VSb1J1eUl4eGFMbmE5ZzFDMHhFd05qUExJTUpKZnFTbm10ZXdMcUQzU3Bid1lKaXhZdkVnQ1pYeFJNSDZWM3dqTVU3QnlyZFYyMGlpdVI3U0pKd0hOcVJwTURvTmQ4TndMRkY5aS9GTUE5RWJQNHJBcjk4ZDh4WnBXcXE1Zk5pV1Y3QnVFOG53bGEwam5EV2NKUTBQZGtmeEFPSWpUTk5nSncyUDFkQ1Z5SERQSk55cUh6SU9pa3lIK0tIbUF0S2dYVnIrZExTT25JL0ZHYzNDQ3J4c3cxaWMrZU1aSXI0RDNyOWlmT0EybXVQUU82VWgrS3ZBbUM3N2VuTDFMOWkwdlBJaStqZmd4SVplbUF6eFlJWG13WXJKUFNoMDYyNWplSDJDWGVyVjJjVmJGaDlCWWJaSFk3VG1EVUZuVkJvT1FLNis0TDRGS2RkbXBMZC9DViswdTE4a28wbDFuTXNnbC9TTW1RTisxemhGWXZ3N0ltTVZtVm5yU25yMGtuRmcxTDFsT2tyckMrbzlacEQwMXBHbldJT2N6RTh6WUcydlpmQlVuaUhmd2pjMmk5Q0VWNXFLdG9RNVB1bXZvaS9WVzNWSFM0aXJYQysvVy9ySjB5VDRTMHVwOGVVWEdVU28rcU84eEREY2o0TlU2TFVsek5lZmQxaTRORWFhR1IxV0pwNHlhNGhtWUUrek9MMTZ3NTBMVG1wM0p0bWFZd3ZWQzloZ0dUd3cxYUpwYmJQdHdXOHlnQ2MrSVYxNjlScStlb2EzcjQxOWlYY0hiVDRzK1poTHBheEpFL1c0NDJpTnZuc3JNeDk2dTRmT3hQVFpMR1ByRTFaR3FOcUlBK1pFNUczejc0Q1drLzBybjdEWmo0M1hGOUtGUjh4QlBMV2x6MXZPaE5CK0FTZ3hmTUxyTUpveXlFdVRXSHZ5RTlCbFRwZUY1ZjB4Nm45a2hhV3loZjFwNlE2ekZ6SVNLWW5XOXhrc1BpekNxaVhXYmxxMG9Lb3hsc0pwOXpGY3dZS3l6NDJnb1Z4SHlYZHJKMHlZS3BzZWhFQnVaNTkvUm5ac0lnM3U1dGtKS24vUHpqTFM3MUwvTGk4S3ZINVlGd0dxcFhIQ2oxMVlFdGVMSEtVWmR5WjBXWFZIWDBHSWhFM0pWdlBVUm5ycEJ1QUdkNEQwSHNhV3ZrckNWUXZ3d3JaMFlPcitnVk9vaGV4dTFBcGVlV1lTZDNvdlJUUGVhbjhGVTl2eVFNUXRBT2c3TXBqS0g2ZEZNOUhMbXJjckJ3K08wOXliNnpmM2pISGYzczVKR3g4WUFBYjFWTHN0MXVCZWY4Q3VxRmVhWVlnMzdhMEVUVjVGQlhraDN3ajkwRnNYamFTdTQrYWlkS0lONnhXU0hQbGZ3TUs0UjRNRUtrYlk4bGdJdkRSckg1cGdhemJUK2RtOTNlR1hnTWJQdjN1Z1RSQlZmaXNhTUpmbUZIYzNpOUxNdUMvOVlRNkNEbmlMUXR0dC9sVnlDclBpOUtEZTlBNERBQjZnb0phN1RHNFdNalNJS1NxZlpmRWY0ODVBWU9GSm5iNHV6ejk4bGtrT0FLTEgyVTg2ZS80WEZmVUY1MUxBUjFPTFlGU0RNdkttdWJxOFc0S0ZWT2oxNnRRU0doNFVlcWVXK0dHcXFzK2ZaaWxiKzlLTk5VYVN5ZWRNbUViZkxIQXRSWkVsUlk4VnpiaUFGdGRhYWF5Z0VPQ0pkUFdsc0hNN3M4ZEJwUWs4bm9WQkNPb2dSZHUzRXgzR3NjOHdMYVlPQWpvZkQ5TDVQc1lpQVpPVDJKOTcrMjYzMS8yLy84a2d1a2VVdkxrOTRNb3lJRFJlSkhsKzM0VVhxZlVpQmduWjl3dW01N1k2WU1DRFNnRms3SlZlMHNQUENDejQxWnVTNjA2UWw1aVVCU2l3YlhZSkUxRzkvZWRmM0I3RTVwRGxhb3dMQ1BMb1JDZGNYVnlmbnJmajQvOExNZUN3WFU0cEJkb0lYMTlKYm1adlgxOXlJUUFBQUhJakFBQUFjaVFBQUFEYUIyTnZiWEJwYkdXcEFISW5BQUFBY2dzQUFBRGFDRHh0YjJSMWJHVStjaXNBQUFBQkFBQUFjMUVBQUFEd0F3RUJBZGdMUDRBSTJBQkFnUVRRQlJmWEJSN1JCUjdUQlNEWEJTZlJCU2ZRS0Q3VEJUL1RBRUR5QUFCREFVa0c4QUFBU3daYVdnUHdBQUJjV2dOaldnUHpBQUJDQVdSYUEvVUFBQUZsV2dOeUp3QUFBQT09JykpCikKClUzUmhjblFnVkdocGN5QnZZbk5qZFhKbElHbHpJR052YlcxdmJtbDBlU0JtWldGMGRYSmxjM1J5YVc1bkxDQmhibVFnYVhRZ1kyRnVibTkwSUdKbElIVnpaV1FnWm5KdmJTQmtaV0ZzYVc1bklHVnVZM0o1Y0gxMjM0NTY3ODkwZmFrZV92YXI0X3BhbHN1X3lnX3RhbXBha19rcnVzaWFsX2JhbmdldA==').decode()
-)
+___author____ = ""
+___infos = f"""#__author__ = "{___author____}"
+#__madeBy__ = ""
+#__git__ = ""
+print("mek")
+"""
+def palsu():
+	palsu = "import os,sys,time,random\n\nprint('')"
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+class Obfuscator:
+    def __init__(self, code):
+        self.code = code
+        self.__obfuscate()
+
+    def __com__fer(self, text, key=None):
+        newstring = ""
+        if key is None:
+            key = "".join(
+                random.choices(
+                    string.digits + string.ascii_letters, k=random.randint(4, 8)
+                )
+            )
+        if not key[0] == " ":
+            key = " " + key
+        for i in range(len(text)):
+            newstring += chr(ord(text[i]) ^ ord(key[(len(key) - 2) + 1]))
+        return (newstring, key)
+
+    def __encodestring(self, string):
+        newstring = ""
+        for i in string:
+            if random.choice([True, False]):
+                newstring += "\\x" + codecs.encode(i.encode(), "hex").decode()
+            else:
+                newstring += "\\" + oct(ord(i))[2:]
+        return newstring
+
+    def __obfuscate(self):
+        compile_f = self.__com__fer(self.code)
+        self.code = compile_f[0]
+        encoded_code = base64.b64encode(
+            codecs.encode(codecs.encode(self.code.encode(), "bz2"), "uu")
+        ).decode()
+        encoded_code = [
+            encoded_code[i : i + int(len(encoded_code) / 4)]
+            for i in range(0, len(encoded_code), int(len(encoded_code) / 4))
+        ]
+        new_encoded_code = []
+        new_encoded_code.append(
+            codecs.encode(encoded_code[0].encode(), "uu").decode() + "u"
+        )
+        new_encoded_code.append(codecs.encode(encoded_code[1], "rot13") + "r")
+        new_encoded_code.append(
+            codecs.encode(encoded_code[2].encode(), "hex").decode() + "h"
+        )
+        new_encoded_code.append(
+            base64.b85encode(codecs.encode(encoded_code[3].encode(), "hex")).decode()
+            + "x"
+        )
+        self.code = f"""
+_____=eval("{self.__encodestring('eval')}")
+_______=_____("{self.__encodestring('compile')}")
+______,____=_____(_______("{self.__encodestring("__import__('base64')")}","",_____.__name__)),_____(_______("{self.__encodestring("__import__('codecs')")}","",_____.__name__));____________________=_____("'{self.__encodestring(compile_f[True])}'");________,_________,__________,___________=_____(_______("{self.__encodestring('exec')}","",_____.__name__)),_____(_______("{self.__encodestring('str.encode')}","",_____.__name__)),_____(_______("{self.__encodestring('isinstance')}","",_____.__name__)),_____(_______("{self.__encodestring('bytes')}","",_____.__name__))
+def bad__marshal__cythonize(__________, ___________):
+    __________=__________.decode()
+    _________=""
+    if not ___________[False]=="{self.__encodestring(' ')}":
+        ___________="{self.__encodestring(' ')}"+___________
+    for _ in range(_____("{self.__encodestring('len(__________)')}")):
+        _________+=_____("{self.__encodestring('chr(ord(__________[_])^ord(___________[(len(___________) - True*2) + True]))')}")
+    return (_________,___________)
+def bad__gateway__malware(_____________):
+    if(_____________[-True]!=_____(_______("'{self.__encodestring('c________________6s5________________6ardv8')}'[-True*4]","",_____.__name__))):_____________ = _________(_____________)
+    if not(__________(_____________, ___________)):_____________ = _____(_______("{self.__encodestring('____.decode(_____________[:-True]')},'{self.__encodestring('rot13')}')","",_____.__name__))
+    else:
+        if(_____________[-True]==_____(_______("b'{self.__encodestring('f5sfsdfauf85')}'[-True*4]","", _____.__name__))):
+            _____________=_____(_______("{self.__encodestring('____.decode(_____________[:-True]')},'{self.__encodestring('uu')}')","",_____.__name__))
+        elif (_____________[-True] ==_____(_______("b'{self.__encodestring('d5sfs1dffhsd8')}'[-True*4]","", _____.__name__))):_____________=_____(_______("{self.__encodestring('____.decode(_____________[:-True]')},'{self.__encodestring('hex')}')","",_____.__name__))
+        else:_____________=_____(_______("{self.__encodestring('______.b85decode(_____________[:-True])')}","",_____.__name__));_____________=_____(_______("{self.__encodestring('____.decode(_____________')}, '{self.__encodestring('hex')}')","",_____.__name__))
+        _____________=_____(_______("{self.__encodestring('___________.decode(_____________)')}","",_____.__name__))
+    return _____________
+_________________=_____(_______("{self.__encodestring('___________.decode')}({self.__encodestring(new_encoded_code[True*3]).encode()})","",_____.__name__));________________ = _____(_______("{self.__encodestring('___________.decode')}({self.__encodestring(new_encoded_code[1]).encode()})","",_____.__name__));__________________=_____(_______("{self.__encodestring('___________.decode')}({self.__encodestring(new_encoded_code[True*2]).encode()})","",_____.__name__));______________=_____(_______("{self.__encodestring('___________.decode')}({self.__encodestring(new_encoded_code[False]).encode()})","",_____.__name__));_______________=_____(_______("{self.__encodestring('str.join')}('', {self.__encodestring('[bad__gateway__malware(x) for x in [______________,________________,__________________,_________________]]')})","", _____.__name__));________(bad__marshal__cythonize(____.decode(____.decode(______.b64decode(_________(_______________)), "{self.__encodestring("uu")}"),"{self.__encodestring("bz2")}"),____________________)[_____("{self.__encodestring('False')}")])\n{imports}\n\n# Yah Di dec , ga bisa buat sc ya makanya decrypt\n# Status Anda Memgemis decrypt awok²"""
+
+
+def __fer__nanta__OBF(file_name, output_file_name):
+    with open(file_name, encoding="utf-8") as file:
+        CODE = file.read()
+    obfuscator = Obfuscator(CODE)
+    with open(output_file_name, "w", encoding="utf-8") as output_file:
+#        output_file.write((Obfuscator(___infos)))
+        for i in range(2):
+               output_file.write(obfuscator.code)
+    print("\033[1;32m" + "\n Successfully Obfuscated!...\033[1;37m")
+    time.sleep(1)
+    exit()
+
+
+try:
+    clear()
+    print(
+        f"""\033[1;36m
+     ____  ____    __    _  _  ___  ____  _____  ____  __  __   
+    (_  _)(  _ \  /__\  ( \( )/ __)( ___)(  _  )(  _ \(  \/  )  
+      )(   )   / /(__)\  )  ( \__ \ )__)  )(_)(  )   / )    (   
+     (__) (_)\_)(__)(__)(_)\_)(___/(__)  (_____)(_)\_)(_/\/\_)  
+
+    \033[1;37mBy \033[1;90m
+    \033[1;37mVersion: \033[1;90m0.2"""
+    )
+
+    file_name = input(
+        "\t\033[1;93m"
+        + "\n Drag the Python File "
+        + "\033[1;90m"
+        + " > "
+        + "\033[0;37m"
+        + f""
+    )
+    file_name = file_name.replace("\\", "/").strip('"')
+    if not file_name.endswith(".py"):
+        print("\n\033[1;31m Only for python scripts\033[1;37m")
+        time.sleep(1)
+        exit()
+
+    output_file_name = input(
+        "\t\033[1;93m"
+        + "\n Enter the Output File Name "
+        + "\033[1;90m"
+        + " > "
+        + "\033[0;37m"
+        + f""
+    )
+
+    bruh = ""
+    try:
+        file_read = open(file_name, "r")
+        text = "import "
+        lines = file_read.readlines()
+        new_list = []
+        idx = 0
+        for line in lines:
+            if text in line:
+                new_list.insert(idx, line)
+                idx += 1
+        file_read.close()
+        lineLen = len(new_list)
+        for i in range(lineLen):
+            if "(" or "." or ")" or "'" in new_list[i].strip():
+                pass
+            else:
+                bruh += new_list[i].strip() + ";"
+    except FileNotFoundError:
+        print("\n The file doesn't exist!")
+        time.sleep(1)
+        exit()
+
+    l = len(bruh)
+    imports = bruh[: l - 1]
+
+    __fer__nanta__OBF(file_name, output_file_name)
+except (KeyboardInterrupt, Exception) as e:
+#    exit(f"[ Erorr ] {str(e).capitalize()} !")
+     exit(e)
